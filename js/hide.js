6f9bf201d0ead7f0bcc9dfb1ec2401d88692748d
yangzheng541 <75741977+yangzheng541@users.noreply.github.com>
2023-12-21 20:50:28 +0800

    hide正确版

--- js/hide.js
+++ js/hide.js
@@ -4,26 +4,15 @@ function hide() {
 }
 
 function verifyStart() {
-    function verify(password) {
-        var result = false;
-        $.getJSON("json/password-digest.json", function(data) {
-            console.log(data);
-            console.log(sm3Digest(password));
-            if (sm3Digest(password) == data["password-digest"]) {
-            console.log("right!")
-                result = true;
-            }
-        });
-        console.log(result)
-        return result;
-    }
-    password = $('#password').val();
-    if(verify(password)){
-        $(".hide").show();
-        $(".verify-card").hide();
-    } else {
-        window.alert("密码验证错误！")
-    }
+    $.getJSON("json/password-digest.json", function(data) {
+        password = $('#password').val();
+        if (sm3Digest(password) == data["password-digest"]) {
+            $(".hide").show();
+            $(".verify-card").hide();
+        } else {
+            window.alert("密码验证错误！")
+        }
+    });
 }
 
 hide();
