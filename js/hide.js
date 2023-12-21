0c2c3fc87815b3d853ade85b10bc1296f5826dc2
yz-love-yxp <154495247+yz-love-yxp@users.noreply.github.com>
2023-12-21 23:56:19 +0800

    首页密码保护可用版本

--- js/hide.js
+++ js/hide.js
@@ -1,18 +1,33 @@
-var password = ""
+var password = "";
+var idArr = ["#menu", "#header", "#page-content", "#footer"];
 function hide() {
-    $(".hide").hide();
+	fadeOut(idArr);
 }
 
 function verifyStart() {
     $.getJSON("json/password-digest.json", function(data) {
         password = $('#password').val();
         if (sm3Digest(password) == data["password-digest"]) {
-            $(".hide").show();
-            $(".verify-card").hide();
+            fadeIn(idArr);
+            $("#verify-card").fadeOut();
+			main();
         } else {
             window.alert("密码验证错误！")
         }
     });
 }
+
+function fadeOut(idArr) {
+	for (let id of idArr) {
+		$(id).fadeOut()
+	}
+}
+
+function fadeIn(idArr) {
+	for (let id of idArr) {
+		$(id).fadeIn()
+	}
+}
+
 
 hide();
